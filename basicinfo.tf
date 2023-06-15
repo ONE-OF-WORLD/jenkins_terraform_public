7ef03ed5260f575ddae2220805242cdaa3606b40
gugucone999 <gugucone999@gmail.com>
2023-06-15 15:09:23 +0900

    test

--- basicinfo.tf
+++ basicinfo.tf
@@ -1,34 +1,23 @@
-#### Basic Information ####
-  
-# Provider Information
-provider "aws" {
-  region = "ap-northeast-2"
-}
-
 terraform {
-  # Library 버전
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.20"
-    }
-    tls = {
-      source = "hashicorp/tls"
+      version = "~> 4.16"
     }
   }
-#
-  # Terraform 버전
-  required_version = ">= 0.13"
 
-  # State 저장용 S3 Backend 구성
-  backend "s3" {
-    bucket = "terraform test" #S3 버킷명
-    key    = "C:\Users\Playdata\Desktop\terraform\jenkins_terraform_public" #terraform.tfstate 파일위치 ex) aws-an2/jenkins_terraform_test/terraform.tfstate
-    region = "ap-northeast-2" # 리전
-    dynamodb_table = "" #테라폼 백엔드 구성용 DynamoDB 테이블명
-    encrypt = true
-  }
+  required_version = ">= 1.2.0"
 }
 
-#### Remote State Data Source ####
-data "aws_availability_zones" "all" {}
+provider "aws" {
+  region  = "ap-northeast-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-068a0feb96796b48d"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
